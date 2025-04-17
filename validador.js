837139c5f6011be80a513cb80f9ef41a84d07ed1
gustavo <gustavo_klahold@estudante.sesisenai.org.br>
2025-04-17 09:25:05 -0300

    add

--- validador.js
+++ validador.js
@@ -1,36 +1,70 @@
-function validarFormulario(){
+function validarFormulario() {
 
     //pegar os dados//
 
-    const nome =document.getElementById('nome_maquinista').value.trim();
+    const nome = document.getElementById('nome').value.trim();
 
-    const cpf =document.getElementById('cpf_maquinista').value.trim();
+    const email = document.getElementById('email').value.trim();
 
-    const idade =parseInt(document.getElementById('idade_maquinista').value, 10);
+    const idade = parseInt(document.getElementById('idade_maquinista').value, 10);
+
+    const genero = document.getElementById('genero').value.trim();
+
+    const comida = document.getElementById('comida').value.trim();
+
+    const corOlho = document.getElementById('corOlho').value.trim();
+
+    const corCabelo = document.getElementById('corCabelo').value.trim();
+
+    const pe = document.getElementById('pe').value.trim();
+
+    const tamanho = document.getElementById('tamanho').value.trim();
+
+    const nomePai = document.getElementById('nomePai').value.trim();
+
+    const nomeMae = document.getElementById('nomeMae').value.trim();
+
+    const irmaos = document.getElementById('irmaos').value.trim();
+
+    const numero = document.getElementById('numero').value.trim();
+
+    const corPele = document.getElementById('corPele').value.trim();
+
+    const cor = document.getElementById('cor').value.trim();
+
+    const geladeira = document.getElementById('geladeira').value.trim();
+
+    const aniversario = document.getElementById('aniversario').value.trim();
+
+    const time = document.getElementById('time').value.trim();
+
+    const materia = document.getElementById('materia').value.trim();
+
+    const nasceu = document.getElementById('nasceu').value.trim();
+
+    const animal = document.getElementById('animal').value.trim();
+
+    const academia = document.getElementById('academia').value.trim();
+
+    const problema = document.getElementById('problema').value.trim();
+
+    const checkbox = document.getElementById('checkbox').value.trim();
+
+    const som = document.getElementById('som').value.trim();
+
+    const tel = document.getElementById('tel').value.trim();
+
+    const senha = document.getElementById('senha').value.trim();
+
+    const carro = document.getElementById('carro').value.trim();
+
+    const amigos = document.getElementById('amigos').value.trim();
+
+    const comer = document.getElementById('comer').value.trim();
 
 
     //processamento//
-
-    if(!nome){
-        alert('Por favor, preencha o nome.');
-        return;
-    }else{
-        console.log(nome);
-    }
-
-    if (!cpf ||  cpf.length !== 11  || isNaN(cpf)) {
-        alert('Por favor, insira um CPF valido com 11 digitos');
-        return;
-    } else {
-        console.log(cpf);
-    }
-
-    if (isNaN(idade) || idade < 21 || idade > 0) {
-        alert('A idade deve ser um numeor maior do que 30 anos');
-        return;
-    } else {
-        console.log(idade);
-    }
+    
 
     //developer//
 
